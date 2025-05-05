597e898b98c80f2cebbeeef34bf8ed5042b9e44a
Reonu <danileon95@gmail.com>
2025-05-05 23:27:42 +0100

    add deku great fairy sword asset

--- src/asset_replacement.c
+++ src/asset_replacement.c
@@ -3,6 +3,7 @@
 #include "gLinkZoraBowSkel.h"
 #include "gLinkZoraHookshotSkel.h"
 #include "gLinkZoraBow3rdPersonSkel.h"
+#include "gGreatFairySword.h"
 
 extern Gfx* sPlayerFirstPersonRightShoulderDLs[PLAYER_FORM_MAX];
 extern Gfx* sPlayerFirstPersonRightHandDLs[PLAYER_FORM_MAX];
@@ -15,6 +16,11 @@ extern Gfx gLinkZoraRightHandClosedDL[];
 extern Gfx* gPlayerRightHandHookshotDLs[2 * PLAYER_FORM_MAX];
 extern Gfx* gPlayerRightHandBowDLs[2 * PLAYER_FORM_MAX];
 
+extern Gfx* gPlayerLeftHandTwoHandSwordDLs[2 * PLAYER_FORM_MAX];
+
+extern Gfx gLinkDekuLeftHandDL[];
+extern Gfx gLinkHumanGreatFairysSwordDL[];
+
 Gfx gLinkZoraRightHandHoldingHookshotDL[] = {
     gsSPDisplayList(gLinkZoraRightHandClosedDL),
     gsSPBranchList(gLinkHumanHookshotDL),
@@ -25,7 +31,13 @@ Gfx gLinkZoraRightHandShoulderAndForearm[] = {
     gsSPBranchList(gLinkZoraBowSkel_bone017_gLinkZoraRightForearmLimb_mesh_layer_Opaque),
 };
 
+Gfx gLinkDekuHoldingGreatFairySwordDL[] = {
+    gsSPDisplayList(gLinkDekuLeftHandDL),
+    gsSPBranchList(gGreatFairySword),    
+};
+
 RECOMP_HOOK("Player_Init") void on_Player_Init(Actor* thisx, PlayState* play) {
+    // Zora link
     sPlayerFirstPersonRightShoulderDLs[PLAYER_FORM_ZORA] = gLinkZoraRightHandShoulderAndForearm;
     sPlayerFirstPersonRightHandDLs[PLAYER_FORM_ZORA] = gLinkZoraBowSkel_bone018_gLinkZoraRightHandLimb_mesh_layer_Opaque;
     gPlayerRightHandHookshotDLs[PLAYER_FORM_ZORA * 2 + 0] = gLinkZoraRightHandHoldingHookshotDL;
@@ -33,5 +45,8 @@ RECOMP_HOOK("Player_Init") void on_Player_Init(Actor* thisx, PlayState* play) {
     gPlayerRightHandBowDLs[PLAYER_FORM_ZORA * 2 + 0] = gLinkZoraBow3rdPersonSkel_bone018_gLinkZoraRightHandLimb_mesh_layer_Opaque;
     gPlayerRightHandBowDLs[PLAYER_FORM_ZORA * 2 + 1] = gLinkZoraBow3rdPersonSkel_bone018_gLinkZoraRightHandLimb_mesh_layer_Opaque;
     sPlayerFirstPersonRightHandHookshotDLs[PLAYER_FORM_ZORA] = gLinkZoraHookshotSkel_bone018_gLinkZoraRightHandLimb_mesh_layer_Opaque;
+
+    // Deku Link
+    gPlayerLeftHandTwoHandSwordDLs[PLAYER_FORM_DEKU * 2 + 0] = gLinkDekuHoldingGreatFairySwordDL;
 }
 
