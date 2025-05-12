d3989f5ae6bbacd543df1e21a553bd85a9792ca1
Reonu <danileon95@gmail.com>
2025-05-12 12:04:17 +0100

    Fix crashes and improve the models

--- src/asset_replacement.c
+++ src/asset_replacement.c
@@ -58,71 +58,61 @@ RECOMP_HOOK("Player_Init") void on_Player_Init(Actor* thisx, PlayState* play) {
     gPlayerLeftHandTwoHandSwordDLs[PLAYER_FORM_DEKU * 2 + 0] = gLinkDekuHoldingGreatFairySwordDL;
 }
 
-extern PlayerAnimationHeader gPlayerAnim_link_hook_shot_ready;
-extern PlayerAnimationHeader gPlayerAnim_pn_tamahakidf;
-extern PlayerAnimationHeader gPlayerAnim_link_bow_bow_ready;
-extern PlayerAnimationHeader gPlayerAnim_link_uma_anim_walk;
-extern PlayerAnimationHeader gPlayerAnim_pz_cutterwaitA;
-extern PlayerAnimationHeader gPlayerAnim_pz_cutterwaitB;
-extern PlayerAnimationHeader gPlayerAnim_pz_cutterwaitC;
+RECOMP_IMPORT("ProxyMM_ObjDepLoader", bool ObjDepLoader_Load(PlayState* play, u8 segment, s16 objectId));
+RECOMP_IMPORT("ProxyMM_ObjDepLoader", void ObjDepLoader_Unload(PlayState* play, u8 segment, s16 objectId));
 
-s32 func_808306F8(Player* this, PlayState* play);
-void Player_AnimationPlayLoop(PlayState* play, Player* this, PlayerAnimationHeader* anim);
-PlayerAnimationHeader* func_8082ED20(Player* this);
-s32 Player_UpperAction_12(Player* this, PlayState* play);
-void Player_SetUpperAction(PlayState* play, Player* this, PlayerUpperActionFunc upperActionFunc);
-
-// Allows use of bow and hookshot as Zora Link:
-RECOMP_PATCH s32 func_80830E30(Player* this, PlayState* play) {
-
-    if (!func_808306F8(this, play)) {
-        return false;
+PlayState* sPlayState;
+static bool sShouldUnloadForPostLimbDraw = false;
+RECOMP_HOOK("Player_PostLimbDrawGameplay") void on_Player_PostLimbDrawGameplay(PlayState* play, s32 limbIndex, Gfx** dList1, Gfx** dList2, Vec3s* rot, Actor* actor) {
+    if (limbIndex == PLAYER_LIMB_RIGHT_HAND) {
+        sShouldUnloadForPostLimbDraw = true;
+        ObjDepLoader_Load(play, 0x6, OBJECT_LINK_CHILD);
     }
-
-    PlayerAnimation_PlayOnce(play, &this->skelAnimeUpper,
-                             Player_IsHoldingHookshot(this)
-                                 ? &gPlayerAnim_link_hook_shot_ready
-                                 : ((this->transformation == PLAYER_FORM_DEKU) ? &gPlayerAnim_pn_tamahakidf
-                                                                               : &gPlayerAnim_link_bow_bow_ready));
-
-
-    if (this->stateFlags1 & PLAYER_STATE1_800000) {
-        Player_AnimationPlayLoop(play, this, &gPlayerAnim_link_uma_anim_walk);
-    } else if ((this->actor.bgCheckFlags & BGCHECKFLAG_GROUND) && (this->transformation != PLAYER_FORM_ZORA)) {
-        Player_AnimationPlayLoop(play, this, func_8082ED20(this));
-    }
-
-    return true;
+    sPlayState = play;
 }
-/*
-s32 func_80830E30_2(Player* this, PlayState* play) {
-    if ((this->heldItemAction == PLAYER_IA_11) || (this->transformation == PLAYER_FORM_ZORA)) {
-        Player_SetUpperAction(play, this, Player_UpperAction_12);
 
-        PlayerAnimation_PlayOnce(play, &this->skelAnimeUpper,
-                                 (this->meleeWeaponAnimation == PLAYER_MWA_ZORA_PUNCH_LEFT)
-                                     ? &gPlayerAnim_pz_cutterwaitA
-                                     : ((this->meleeWeaponAnimation == PLAYER_MWA_ZORA_PUNCH_COMBO)
-                                            ? &gPlayerAnim_pz_cutterwaitB
-                                            : &gPlayerAnim_pz_cutterwaitC));
-        this->unk_ACC = 0xA;
-    } else {
-        if (!func_808306F8(this, play)) {
-            return false;
+RECOMP_HOOK_RETURN("Player_PostLimbDrawGameplay") void return_PostLimbDrawGameplay(void) {
+    if (sShouldUnloadForPostLimbDraw) {
+        sShouldUnloadForPostLimbDraw = false;
+        ObjDepLoader_Unload(sPlayState, 0x6, OBJECT_LINK_CHILD);
+    }
+}
+
+static bool sShouldUnloadForArmsHook = false;
+RECOMP_HOOK("ArmsHook_Draw") void on_ArmsHook_Draw(Actor* thisx, PlayState* play) {
+    sShouldUnloadForArmsHook = true;
+    ObjDepLoader_Load(play, 0x6, OBJECT_LINK_CHILD);
+    sPlayState = play;
+}
+
+RECOMP_HOOK_RETURN("ArmsHook_Draw") void return_ArmsHook_Draw(void) {
+    if (sShouldUnloadForArmsHook) {
+        sShouldUnloadForArmsHook = false;
+        ObjDepLoader_Unload(sPlayState, 0x6, OBJECT_LINK_CHILD);
+    }
+}
+
+Player *gOriginalPlayer;
+u8 gOriginalPlayerTransformation;
+u8 gShouldSkipFins = false;
+RECOMP_HOOK("func_80126BD0") void on_func_80126BD0(PlayState* play, Player* player, s32 arg2) {
+    gOriginalPlayer = player;
+    gOriginalPlayerTransformation = player->transformation;
+    if (gShouldSkipFins) {
+        if (player->transformation == PLAYER_FORM_ZORA) {
+            player->transformation = PLAYER_FORM_HUMAN;
         }
-
-        PlayerAnimation_PlayOnce(play, &this->skelAnimeUpper,
-                                 (Player_IsHoldingHookshot(this))
-                                     ? &gPlayerAnim_link_hook_shot_ready
-                                     : ((this->transformation == PLAYER_FORM_DEKU) ? &gPlayerAnim_pn_tamahakidf
-                                                                                   : &gPlayerAnim_link_bow_bow_ready));
     }
+}
 
-    if (this->stateFlags1 & PLAYER_STATE1_800000) {
-        Player_AnimationPlayLoop(play, this, &gPlayerAnim_link_uma_anim_walk);
-    } else if ((this->actor.bgCheckFlags & BGCHECKFLAG_GROUND) && (this->transformation != PLAYER_FORM_ZORA)) {
-        Player_AnimationPlayLoop(play, this, func_8082ED20(this));
+RECOMP_HOOK_RETURN("func_80126BD0") void return_func_80126BD0(void) {
+    if (gShouldSkipFins) {
+        gOriginalPlayer->transformation = gOriginalPlayerTransformation;
+        gShouldSkipFins = false;
     }
+    
+}
 
-    return true;
-}*/
+RECOMP_HOOK("Player_OverrideLimbDrawGameplayFirstPerson") void on_Player_OverrideLimbDrawGameplayFirstPerson(PlayState* play, s32 limbIndex, Gfx** dList, Vec3f* pos, Vec3s* rot, Actor* actor) {
+    gShouldSkipFins = true;
+}
--- src/modified_item_restriction_table.c
+++ src/modified_item_restriction_table.c
@@ -8,6 +8,46 @@
 extern u8 gPlayerFormItemRestrictions[PLAYER_FORM_MAX][114];
 
 // Patches a function in the base game that's used to check if the player should quickspin.
+
+extern void Player_SetUpperAction(PlayState* play, Player* this, PlayerUpperActionFunc upperActionFunc);
+extern void PlayerAnimation_PlayOnce(PlayState* play, SkelAnime* skelAnime, PlayerAnimationHeader* animation);
+extern s32 func_808306F8(Player* this, PlayState* play);
+extern s32 Player_IsHoldingHookshot(Player* player);
+extern void Player_AnimationPlayLoop(PlayState* play, Player* this, PlayerAnimationHeader* anim);
+extern s32 Player_UpperAction_12(Player* this, PlayState* play);
+extern LinkAnimationHeader gPlayerAnim_pz_cutterwaitB;
+extern LinkAnimationHeader gPlayerAnim_pz_cutterwaitA;
+extern LinkAnimationHeader gPlayerAnim_pz_cutterwaitC;
+extern LinkAnimationHeader gPlayerAnim_link_hook_shot_ready;
+extern LinkAnimationHeader gPlayerAnim_pn_tamahakidf;
+extern LinkAnimationHeader gPlayerAnim_link_bow_bow_ready;
+extern LinkAnimationHeader gPlayerAnim_link_uma_anim_walk;
+extern PlayerAnimationHeader* func_8082ED20(Player* this);
+
+
+
+RECOMP_PATCH s32 func_80830E30(Player* this, PlayState* play) {
+
+        if (!func_808306F8(this, play)) {
+            return false;
+        }
+
+        PlayerAnimation_PlayOnce(play, &this->skelAnimeUpper,
+                                 Player_IsHoldingHookshot(this)
+                                     ? &gPlayerAnim_link_hook_shot_ready
+                                     : ((this->transformation == PLAYER_FORM_DEKU) ? &gPlayerAnim_pn_tamahakidf
+                                                                                   : &gPlayerAnim_link_bow_bow_ready));
+    
+
+    if (this->stateFlags1 & PLAYER_STATE1_800000) {
+        Player_AnimationPlayLoop(play, this, &gPlayerAnim_link_uma_anim_walk);
+    } else if ((this->actor.bgCheckFlags & BGCHECKFLAG_GROUND) && (this->transformation != PLAYER_FORM_ZORA)) {
+        Player_AnimationPlayLoop(play, this, func_8082ED20(this));
+    }
+
+    return true;
+}
+
 RECOMP_CALLBACK("*", recomp_after_play_init) void modify_item_restriction_table(PlayState* this) {
 
     // Deku Scrub
