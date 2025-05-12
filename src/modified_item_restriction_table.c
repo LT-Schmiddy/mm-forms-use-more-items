672d9608f059ff3d095f045e8345bb58aee22d8b
LT-Schmiddy <schmidalex1996@gmail.com>
2025-05-12 18:52:09 -0400

    Menu refined. Added entries for FD Link.

--- src/modified_item_restriction_table.c
+++ src/modified_item_restriction_table.c
@@ -3,13 +3,11 @@
 #include "recomputils.h"
 
 #include "overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_scope.h"
+RECOMP_IMPORT("*", void recomp_set_fd_anywhere(bool new_val));
 
 extern u8 gPlayerFormItemRestrictions[PLAYER_FORM_MAX][114];
 extern u8 gPlayerFormSlotRestrictions[PLAYER_FORM_MAX][ITEM_NUM_SLOTS];
 
-
-
-static u8 overwrite_PlayerFormItemRestrictions[PLAYER_FORM_MAX][114];
 static u8 overwrite_PlayerFormSlotRestrictions[PLAYER_FORM_MAX][ITEM_NUM_SLOTS];
 
 // Helper functions:
@@ -20,14 +18,10 @@ void set_item_restriction(PlayerTransformation transform, ItemId item, u8 allowe
     
 }
 
-
-
-
-
+// Hooks:
 RECOMP_HOOK("KaleidoScope_Update") void set_menu_allowed_ammo(PlayState* this){
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 24; j++) {
-            gPlayerFormItemRestrictions[i][j] = overwrite_PlayerFormItemRestrictions[i][j];
             gPlayerFormSlotRestrictions[i][j] = overwrite_PlayerFormSlotRestrictions[i][j];
         }
     }
@@ -36,7 +30,6 @@ RECOMP_HOOK("KaleidoScope_Update") void set_menu_allowed_ammo(PlayState* this){
 RECOMP_CALLBACK("*", recomp_after_play_init) void modify_item_restriction_table(PlayState* this) {
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 24; j++) {
-            overwrite_PlayerFormItemRestrictions[i][j] = gPlayerFormItemRestrictions[i][j];
             overwrite_PlayerFormSlotRestrictions[i][j] = gPlayerFormSlotRestrictions[i][j];
         }
     }
@@ -63,7 +56,15 @@ RECOMP_CALLBACK("*", recomp_after_play_init) void modify_item_restriction_table(
     set_item_restriction(PLAYER_FORM_ZORA, ITEM_BOW_ICE, 1);
     set_item_restriction(PLAYER_FORM_ZORA, ITEM_BOW_LIGHT, 1);
     set_item_restriction(PLAYER_FORM_ZORA, ITEM_HOOKSHOT, 1);
-    set_item_restriction(PLAYER_FORM_ZORA, ITEM_SWORD_GREAT_FAIRY, 1);
+
+    set_item_restriction(PLAYER_FORM_FIERCE_DEITY, ITEM_BOMB, 1);
+    set_item_restriction(PLAYER_FORM_FIERCE_DEITY, ITEM_BOMBCHU, 1);
+    set_item_restriction(PLAYER_FORM_FIERCE_DEITY, ITEM_DEKU_NUT, 1);
+    set_item_restriction(PLAYER_FORM_FIERCE_DEITY, ITEM_MAGIC_BEANS, 1);
+    set_item_restriction(PLAYER_FORM_FIERCE_DEITY, ITEM_LENS_OF_TRUTH, 1);
+    set_item_restriction(PLAYER_FORM_FIERCE_DEITY, ITEM_POWDER_KEG, 1);
+
+    // recomp_set_fd_anywhere(true);
 
     recomp_printf("Item Restrictions Updated.\n");
 }
